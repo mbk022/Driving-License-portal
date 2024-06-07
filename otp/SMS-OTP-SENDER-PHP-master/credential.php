df33cae9a0d7b1a0e09a93ae937f5072e4280b31
Mubarak <72291169+mbk022@users.noreply.github.com>
2024-06-07 11:38:59 +0530

    Add files via upload

---
+++ otp/SMS-OTP-SENDER-PHP-master/credential.php
@@ -0,0 +1,8 @@
+<?php
+//Text Local
+//Author: Sourabh Barnwal
+//your API Will pe here 
+define("API_KEY",'your api');
+define("MOBILE",'6309533630');
+
+?>
---
+++ otp/send-mobile-otp-php-master/credential.php
@@ -0,0 +1,7 @@
+<?php 
+	/*Enter your API_KEY*/
+	define("API_KEY", 'your api');
+
+	/*You can enter mobile number here*/
+	define("MOBILE", '');
+ ?>
